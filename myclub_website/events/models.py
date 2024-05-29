94f5758a97d5e835e7fc31d657bbf3027c6a2fee
Fabian <fabianarturo2001@gmail.com>
2024-05-28 19:49:00 -0600

    Modify to Django Admin

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,12 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField('Zip Code', max_length=15)
-    phone = models.CharField('Contact Phone', max_length=20)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=20, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address = models.EmailField('Email Address',  blank=True)
     
     def __str__(self):
         return self.name
@@ -22,9 +24,9 @@ class MyClubUser(models.Model):
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date = models.DateField('Event Date')
-    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE) #Eliminar todo lo relacionado
     #venue = models.CharField(max_length=120)
-    manager = models.CharField(max_length=100)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL) #Cambiar a null el campo manager si se elimina
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
     
